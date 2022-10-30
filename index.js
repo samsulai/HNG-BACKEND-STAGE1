987516d82fdf743c22ca0a0ca8af5cdcae560519
samsulai <samuelsulai2015@gmail.com>
2022-10-30 12:14:51 +0100

    typo changes

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => res.json({slackUsername : "sammycodes",
 backend : true,
 age:25,
-bio:"a passionate developer looking forneveryb possible means to improve his knowledge."}));
+bio:"a passionate developer looking for every possible means to improve his knowledge."}));
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
